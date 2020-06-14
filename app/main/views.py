fe9518a05be87bcc7a3a84a3ae9f161cf1d635ee
Paulwamaria <paulwamaria@gmail.com>
2020-06-14 18:03:44 +0300

    Create a sources route

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, url_for, request, redirect
 from . import main
-from ..request import get_news
+from ..request import get_news, get_sources,get_top_headlines
 
 @main.route('/')
 def index():
@@ -10,6 +10,20 @@ def index():
     title = 'World top news'
     message = 'Welcome to world top news'
 
-    news_articles = get_news('business')
+    business_articles = get_news('business')
+    news_sources = get_sources()
+    top_headlines = get_top_headlines("technology")
+   
 
-    return render_template('index.html',title = title, message = message,business_news = news_articles)
+    return render_template('index.html',title = title, message = message,business_articles = business_articles, news_sources = news_sources,top_headlines=top_headlines)
+
+@main.route('/sources')
+def sources():
+    '''
+    Sources view page
+    '''
+    title='news sources'
+
+    sources_list = get_sources()
+
+    return render_template('source.html',title=title,sources = sources_list)
