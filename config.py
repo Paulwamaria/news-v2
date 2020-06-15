52abf358c4057e6bd620fe7207e260e65bbe96e8
Paulwamaria <paulwamaria@gmail.com>
2020-06-15 10:36:44 +0300

    Change API_KEY to NEWS_API_KEY

--- config.py
+++ config.py
@@ -5,12 +5,11 @@ class Config:
     The parent configuration class
     '''
     SECRECT_KEY=os.environ.get('SECRET_KEY')
-    API_KEY=os.environ.get('API_KEY')
-
+    NEWS_API_KEY=os.environ.get('NEWS_API_KEY')
     NEWS_SOURCES_BASE_URL='https://newsapi.org/v2/sources?apikey={}'
     ARTICLES_BASE_URL='https://newsapi.org/v2/everything?q={}&apikey={}'
-    TOP_HEADLINE_BASE_URL='https://newsapi.org/v2/top-headline?category={}&apikey={}'
-
+    TOP_HEADLINE_BASE_URL='https://newsapi.org/v2/top-headlines?category={}&apikey={}'
+    
 
 
 class ProdConfig(Config):
