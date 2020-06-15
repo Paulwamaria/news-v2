bd9deb70873db7103ed44a1544afd62b3f518269
Paulwamaria <paulwamaria@gmail.com>
2020-06-15 10:26:38 +0300

    Add more routes to common news categories

--- app/main/views.py
+++ app/main/views.py
@@ -15,7 +15,7 @@ def index():
     top_headlines = get_top_headlines("technology")
 
     search_term = request.args.get('search_query')
-    print("*"*20,search_term)
+   
     
     if search_term:
         return redirect(url_for('.search',search_term = search_term))
@@ -42,3 +42,97 @@ def search(search_term):
     message = 'Found the following articles '
 
     return render_template('search.html',news_articles = searched_news)
+
+@main.route('/technology')
+def tec_news():
+    '''
+    The view to get technology news
+    '''
+    tech_news = get_news('technology')
+
+    title = 'Technology'
+    return render_template('news.html',title=title,news=tech_news)
+
+@main.route('/sports')
+def sports_news():
+    '''
+    The view to get sports news
+    '''
+    sports_news = get_news('sports')
+
+    title = 'Sports'
+    return render_template('news.html',title=title,news=sports_news)
+
+@main.route('/health')
+def health_news():
+    '''
+    The view to get health news
+    '''
+    health_news = get_news('health')
+
+    title = 'Health'
+    return render_template('news.html',title=title,news=health_news)
+
+@main.route('/politics')
+def politics_news():
+    '''
+    The view to get politics news
+    '''
+    politics_news = get_news('politics')
+
+    title = 'Politics'
+    return render_template('news.html',title=title,news=politics_news)
+
+@main.route('/science')
+def science_news():
+    '''
+    The view to get science news
+    '''
+    science_news = get_news('science')
+
+    title = 'Science'
+    return render_template('news.html',title=title,news=science_news)
+
+
+@main.route('/education')
+def education_news():
+    '''
+    The view to get education news
+    '''
+    education_news = get_news('education')
+
+    title = 'Education'
+    return render_template('news.html',title=title,news=education_news)
+
+
+@main.route('/entertainment')
+def entertainment_news():
+    '''
+    The view to get entertainment news
+    '''
+    entertainment_news = get_news('entertainment')
+
+    title = 'Entertainment'
+    return render_template('news.html',title=title,news=entertainment_news)
+
+@main.route('/entrepreneureship')
+def entrepreneureship_news():
+    '''
+    The view to get entrepreneureship news
+    '''
+    politics_news = get_news('entrepreneureship')
+
+    title = 'Entrepreneureship'
+    return render_template('news.html',title=title,news=entrepreneureship_news)
+
+@main.route('/food')
+def food_news():
+    '''
+    The view to get politics news
+    '''
+    food_news = get_news('food')
+
+    title = 'Food'
+    return render_template('news.html',title=title,news=food_news)
+
+
